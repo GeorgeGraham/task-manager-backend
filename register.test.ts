07032639efee09280066cf07d90e183072a1039f
George Graham <grahamgeorge662@gmail.com>
2025-10-13 18:03:03 +0100

    feature(auth): add simple API test with supertest for register route

---
+++ register.test.ts
@@ -0,0 +1,14 @@
+
+
+import request from "supertest";
+import app from "./index";
+
+describe("POST /register", ()=>{
+  it("should be success with new user", async () => {
+    const res = await request(app)
+      .post("/register")
+      .send({ username: "Bob", password: "password123" });
+      
+    expect(res.statusCode).toBe(201);
+  });
+})
