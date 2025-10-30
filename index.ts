8636cd1a1b3a011dcbf8b2642416a22594b9905b
George Graham <grahamgeorge662@gmail.com>
2025-10-30 17:24:58 +0000

    refactor: now exports createApp , to allow for user repo injection

--- index.ts
+++ index.ts
@@ -1,52 +1,66 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { MockUserRepository } from "./mockUserRepository";
 import { authenticateUser, registerUser } from "./authService";
 import { User } from "./user";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { authenticateToken } from "./authService";
+import { UserRepository } from "./userRepository";
 
-const app = express();
-app.use(express.json());
 
-const userRepo = new MockUserRepository();
 
-// Test route
-app.get("/", (req , res) => {
-  res.send("Task Manager API is running...");
-});
+//To allow for dependency injection
+function createApp(userRepo : UserRepository ){
+  //Load Environment Variables
+  dotenv.config();
 
-app.post("/register", async (req, res) => {
-  const { name, password } = req.body;
-  const newUser = new User(name, password);
-  registerUser(newUser, userRepo).then(() => {
-    res.status(201).json({ message: "User registered successfully" });
-  }).catch((err) => {
-    res.status(400).json({ error: err.message });
+  //Secret for JWT
+  const secret = process.env.SECRET;
+
+  const app = express();
+  app.use(express.json());
+
+  // Test route
+  app.get("/",authenticateToken, (req , res) => {
+    res.send("Task Manager API is running...");
   });
-});
 
-app.post("/login", async (req, res)=>{
-  const {name, password} = req.body;
-  let result =  await authenticateUser(name,password,userRepo);
-  const crypto = require("crypto");
-  const sessionId = crypto.randomBytes(32).toString("base64url");
-  //Store Hash
-  const sessionHash = crypto.createHash('sha256').update(sessionId).digest('hex');
-  res.cookie('sessionId',sessionHash, {
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-    maxAge: 24*60*60*1000
+
+  app.post("/register", async (req, res) => {
+    const { name, password } = req.body;
+    const newUser = new User(name, password);
+    registerUser(newUser, userRepo).then(() => {
+      res.status(201).json({ message: "User registered successfully" });
+    }).catch((err) => {
+      res.status(400).json({ error: err.message });
+    });
+  });
+
+  app.post("/login", async (req, res)=>{
+    const {username, password} = req.body;
+    
+    let authenticated =  await authenticateUser(username,password,userRepo);
+    console.log(authenticated);
+    //console.log("Cooking");
+    //if(!authenticated){return res.status(401).send('Invalid credcentials')}
+    //const token = generateAccessToken(username);
+    //res.json(token);
+    res.status(200).send();
   })
-  res.status(200).send("User Logged In Successfully");
-})
 
-app.post("/logout",(req,res)=>{
-  res.cookie('sessionId',"", {
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-    expires: new Date(0),
+  app.post("/logout",(req,res)=>{
+    res.cookie('sessionId',"", {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+      expires: new Date(0),
+    })
+    res.status(200).end();1
   })
-  res.status(200).end();1
-})
+  return app;
+}
 
-export default app;
+
+
+
+export default createApp;
