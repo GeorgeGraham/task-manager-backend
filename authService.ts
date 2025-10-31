5263424f8860f4b4cb1c0fbd315a4afd3542d89a
George Graham <grahamgeorge662@gmail.com>
2025-10-31 18:13:46 +0000

    fix: fix wrong name for environment variable in authService.ts

--- authService.ts
+++ authService.ts
@@ -42,11 +42,10 @@ export interface AuthRequest extends Request {
 export function authenticateToken(req : AuthRequest, res : Response, next : NextFunction) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1]
-
+  
   if (token == null) return res.sendStatus(401)
 
-  jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-    console.log(err)
+  jwt.verify(token, process.env.SECRET as string, (err: any, user: any) => {
 
     if (err) return res.sendStatus(403)
 
