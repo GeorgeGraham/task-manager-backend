1ee0c605808cc7c9a2fe827d940afa7bf3402a32
George Graham <grahamgeorge662@gmail.com>
2025-10-31 16:35:45 +0000

    refactor: move Creation of mockuserrepository to server.ts from index.ts using createApp function

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 
-import app from "./index";
+
+import createApp from "./index";
+import { MockUserRepository } from "./mockUserRepository";
 
 const PORT = process.env.PORT || 5000;
+
+const userRepository = new MockUserRepository();
+const app = createApp(userRepository)
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
