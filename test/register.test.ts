6c74525b047c6c15333f1dc2e007136d9420fdd9
George Graham <grahamgeorge662@gmail.com>
2025-10-31 16:37:14 +0000

    refactor: Modified register test to use new createApp method

--- test/register.test.ts
+++ test/register.test.ts
@@ -2,10 +2,12 @@
 
 import request from "supertest";
 import app from "../index";
+import createApp from "../index";
+import { MockUserRepository } from "../mockUserRepository";
 
 describe("POST /register", ()=>{
   it("should be success with new user", async () => {
-    const res = await request(app)
+    const res = await request(createApp(new MockUserRepository()))
       .post("/register")
       .send({ username: "Bob", password: "password123" });
       
