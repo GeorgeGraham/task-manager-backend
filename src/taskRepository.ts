7e7d02eba8145fbed03f16c3ae409ce46e9f1e0c
George Graham <grahamgeorge662@gmail.com>
2025-11-12 17:46:08 +0000

    fix: rename method parameter in TaskRepository interface
    renamed username to id , as it was supposed to be id

--- src/taskRepository.ts
+++ src/taskRepository.ts
@@ -6,7 +6,7 @@ export interface TaskRepository {
     //Load Task into Data Store
     createTask(task : Task): Promise<Task>;
     //Get all of a users tasks via their UUID
-    getUsersTasks(username : string) : Promise<Array<Task> | null>;
+    getUsersTasks(id : string) : Promise<Array<Task> | null>;
     //Get Task Via ID
     getTaskByID(id : string) : Promise<Task | null>;
 }
