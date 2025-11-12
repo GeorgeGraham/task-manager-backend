f3695b0a198ebe618c0b8830530b608bbb37e523
George Graham <grahamgeorge662@gmail.com>
2025-11-12 12:57:47 +0000

    feature(crud): Add userId to task as foreign key to track creator

--- src/models/task.ts
+++ src/models/task.ts
@@ -2,8 +2,10 @@
 export class Task{
     id : string;
     title : string;
-    constructor(id : string, title :string){
+    userId : string;
+    constructor(id : string, title :string , userId : string){
         this.id = id;
         this.title = title;
+        this.userId = userId;
     }
 }
