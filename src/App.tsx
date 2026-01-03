3c2563328a0148b4e590978787550343277cb99c
George Graham <grahamgeorge662@gmail.com>
2026-01-03 17:31:34 +0000

    Login-Page Feature Merged , Basic working Login / Register

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import LoginPage from './pages/LoginPage'
-import TasksPage from './pages/TasksPage'
+import RegisterPage from './pages/RegisterPage'
 import './App.css'
+import { Route , Routes } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <TasksPage />
-    </>
+    <Routes>
+      <Route path="/" element={<LoginPage/>} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   )
 }
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router';
-function LoginPage() {
+function RegisterPage() {
   const [count, setCount] = useState(0)
   
-  const handleLogin = () => {
+  const handleRegister = () => {
     console.log("Handling Login Click!");
     let usernameInput = document.getElementById("usernameInput") as HTMLInputElement;
     let passwordInput = document.getElementById("passwordInput") as HTMLInputElement;
     let username = usernameInput.value;
     let password = passwordInput.value;
     console.log("POSTING");
-    axios.post('http://localhost:5000/login',{username:username , password:password})
+    axios.post('http://localhost:5000/register',{username:username , password:password})
     .then(function(response){
       //handle success
       console.log(response);
@@ -30,21 +30,20 @@ function LoginPage() {
       <div className="bg-red-500 p-8 rounded-2xl flex flex-col">
 
         <div className="w-full flex items-center justify-center p-8">
-          <h1 className="text-5xl">Login</h1>
+          <h1 className="text-5xl">Register</h1>
         </div>
+
         <span className="text-2xl">Username</span>
         <input id="usernameInput" className="bg-white p-2 w-80"></input>
         <span className="text-2xl">Password</span>
         <input type="password" id="passwordInput" className="bg-white p-2 w-80"></input>
-        <button onClick={handleLogin}className="bg-red-300 hover:bg-red-700 hover:cursor-pointer text-2xl p-1 mt-8">Login</button>
-        
+        <button onClick={handleRegister}className="bg-red-300 hover:bg-red-700 hover:cursor-pointer text-2xl p-1 mt-8">Register</button>
         <div className="flex items-center flex-col m-2">
-          <span>Don't have an account, <Link className="text-blue-700 font-bold" to="/register">Register</Link></span>
+          <span>Already have an account, <Link className="text-blue-700 font-bold" to="/">Login</Link></span>
         </div>
-        
       </div>
     </div>
   )
 }
 
-export default LoginPage
+export default RegisterPage
